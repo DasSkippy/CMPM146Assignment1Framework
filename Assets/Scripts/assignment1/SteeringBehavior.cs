108f6e1ccf02908e28f95d89f90547be1d410489
Allexindir Smith <asmith67@ucsc.edu>
2025-04-17 12:05:15 -0700

    added initial logic for path following

--- Assets/Scripts/assignment1/SteeringBehavior.cs
+++ Assets/Scripts/assignment1/SteeringBehavior.cs
@@ -7,6 +7,7 @@ public class SteeringBehavior : MonoBehaviour
     public Vector3 target;
     public KinematicBehavior kinematic;
     public List<Vector3> path;
+    public float wpthreshold = 1.0f;
     // you can use this label to show debug information,
     // like the distance to the (next) target
     public TextMeshProUGUI label;
@@ -22,41 +23,59 @@ public class SteeringBehavior : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        // Assignment 1: If a single target was set, move to that target
-        //                If a path was set, follow that path ("tightly")
-        //calculates the distance from car to target
-        float dist = (target - transform.position).magnitude;
-        //calculates the vector direction from the car to the target
-        Vector3 dir = target - transform.position;
-        //calculates the angle from the front of the car to the target
-        float angle = Vector3.SignedAngle(transform.forward, dir, Vector3.up);
-        //labels the dist and angle variables to text in game
-        label.text = dist.ToString() + " " + angle.ToString();
+    {   
 
-        //calls KinematicBehavior to set the rotational velocity to the angle squared
-        //calls KinematicBehavior to set the speed to its max speed
-        if(dist > 20)
-        {
-            kinematic.SetDesiredSpeed(kinematic.GetMaxSpeed());
-            kinematic.SetDesiredRotationalVelocity(angle * angle * Mathf.Sign(angle));
+        if (path != null && path.Count > 0) {
+
+            Vector3 waypoint = path[0];
+            float waypointDist = Vector3.Distance(waypoint, transform.position);
+            target = waypoint;
+
+            if (waypointDist < wpthreshold) {
+                path.RemoveAt(0);
+                if (path.Count == 0) {
+                    path = null;
+                }
+            }
+
+        } else {
+            // Assignment 1: If a single target was set, move to that target
+            //                If a path was set, follow that path ("tightly")
+            //calculates the distance from car to target
+            float dist = (target - transform.position).magnitude;
+            //calculates the vector direction from the car to the target
+            Vector3 dir = target - transform.position;
+            //calculates the angle from the front of the car to the target
+            float angle = Vector3.SignedAngle(transform.forward, dir, Vector3.up);
+            //labels the dist and angle variables to text in game
+            label.text = dist.ToString() + " " + angle.ToString();
+
+            //calls KinematicBehavior to set the rotational velocity to the angle squared
+            //calls KinematicBehavior to set the speed to its max speed
+            if(dist > 20)
+            {
+                kinematic.SetDesiredSpeed(kinematic.GetMaxSpeed());
+                kinematic.SetDesiredRotationalVelocity(angle * angle * Mathf.Sign(angle));
+            }
+            if (dist > 1)
+            {
+                kinematic.SetDesiredSpeed(kinematic.GetMaxSpeed() * dist / 20);
+                kinematic.SetDesiredRotationalVelocity(kinematic.GetMaxRotationalVelocity() * Mathf.Sign(angle));
+            }
+            else
+            {
+                kinematic.SetDesiredSpeed(0);
+            }
+            // you can use kinematic.SetDesiredSpeed(...) and kinematic.SetDesiredRotationalVelocity(...)
+            //    to "request" acceleration/decceleration to a target speed/rotational velocity
         }
-        if (dist < 20)
-        {
-            kinematic.SetDesiredSpeed(kinematic.GetMaxSpeed() * dist / 20);
-            kinematic.SetDesiredRotationalVelocity(kinematic.GetMaxRotationalVelocity() * Mathf.Sign(angle));
-        }
-        if (dist < 1)
-        {
-            kinematic.SetDesiredSpeed(0);
-        }
-        // you can use kinematic.SetDesiredSpeed(...) and kinematic.SetDesiredRotationalVelocity(...)
-        //    to "request" acceleration/decceleration to a target speed/rotational velocity
+        kinematic.setTargetPosition(target);
     }
 
     public void SetTarget(Vector3 target)
     {
         this.target = target;
+        path = null;
         EventBus.ShowTarget(target);
     }
 
